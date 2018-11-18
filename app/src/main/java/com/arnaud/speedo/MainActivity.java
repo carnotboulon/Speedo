28790989ff57b212c9842a760c2649114fd1027a
carnotboulon <arnaudboland@gmail.com>
2018-11-18 22:32:52 +0100

    Settings - almost working. OnBackPressed call does not work. Ok for the rest.

--- app/src/main/java/com/arnaud/speedo/MainActivity.java
+++ app/src/main/java/com/arnaud/speedo/MainActivity.java
@@ -3,6 +3,7 @@ package com.arnaud.speedo;
 
 //import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
@@ -12,9 +13,11 @@ import android.os.Bundle;
 
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.os.SystemClock;
@@ -98,13 +101,51 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     }
 
+    //Open the settings page
+    public void edit_settings() {
+        Log.d("YOYO", "Opening Settings");
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivityForResult(intent,1);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d("YOYO", "On activity result");
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if(resultCode == RESULT_OK) {
+                String target_speed = data.getStringExtra("target_speed");
+                String gps_update_time = data.getStringExtra("gps_update_time");
+                String gps_update_dist = data.getStringExtra("gps_update_dist");
+
+                Log.d("YOYO", "User Target Speed : %s" + target_speed);
+                Log.d("YOYO", "GPS Update time : %s" + gps_update_time);
+                Log.d("YOYO", "GPS Update Distance : %s" + gps_update_dist);
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main_menu, menu);
-        return super.onCreateOptionsMenu(menu);
+        return true;
+        //return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Log.d("YOYO","Settings button clicked!");
+                edit_settings();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
 
     @Override
     public void onLocationChanged(Location location) {
---
+++ app/src/main/java/com/arnaud/speedo/SettingsActivity.java
@@ -0,0 +1,47 @@
+package com.arnaud.speedo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Log.d("YOYO", "In settings activity");
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        Log.d("YOYO", "Go back to parent activity.");
+        Toast.makeText(this,"onBackPressed",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent();
+
+        EditText target_speed_field = findViewById(R.id.user_target_speed);
+        String target_speed = target_speed_field.getText().toString();
+        intent.putExtra("target_speed", target_speed);
+        Log.d("YOYO", "target_speed from Settings: %s" + target_speed);
+
+        EditText gps_update_time_field = findViewById(R.id.user_gps_update_time);
+        String gps_update_time = gps_update_time_field.getText().toString();
+        intent.putExtra("gps_update_time", gps_update_time);
+        Log.d("YOYO", "gps_update_time from Settings: %s" + gps_update_time);
+
+        EditText gps_update_dist_field = findViewById(R.id.user_gps_update_dist);
+        String gps_update_dist = gps_update_dist_field.getText().toString();
+        intent.putExtra("gps_update_dist", gps_update_dist);
+        Log.d("YOYO", "gps_update_dist from Settings: %s" + gps_update_dist);
+
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+
+}
